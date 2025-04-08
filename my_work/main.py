fcc9199018489deb0d7d8ab605bbb8af0db2f809
Owen.L <126544023+OwenLeigh@users.noreply.github.com>
2025-04-08 05:20:48 +0000

    yes yes yes done

--- my_work/main.py
+++ my_work/main.py
@@ -1,21 +1,33 @@
 import csv
 import sys
 
-def menuinputs():
+def Menu_inputs():
+    def Menu_prompt():
+        Menu = input(" do you want to register, login or quit?") #name error (name Menu is not defined)
+        print (Menu)
 
-    Menu = input(" do you want to register, login or quit?")
-    print (Menu)
+#Menu Login Function
 if Menu == Login:
     def userandpasswordcheck():
             User = input("Username")
             Pswrd = input("Password")
-if User != grogu:
+if User != grogu:    #not done fully needs to work on any of the plain_text.txt list
     print('Incorrect Username')
 elif Pswrd != patu:
     print("incorrect Password")
 else: 
-    print("Welcome In!")
+    print("Welcome in")
+    Change = input("do you want to change password?")
+    logout = input("do you want to logout?")
+    def Logout():
+        if logout == Yes:
+            print("Logging out")
+    def PswrdChange():
+        if Change == Yes:  #unfinished passwprd change 
+            print("what do you want to change it to?")
 
+
+#Register Function
 if Menu == register:
     def makenewaccount():
         NewUser= input("What do you want your Username to be?")
@@ -27,9 +39,10 @@ if Menu == register:
                 print ("password has to be longer than 4 characters")
         else:
             print("Username And Password set")
-            print("Try Logging in")
-            #Save new Username and Password 
+            print(Menu)
+            
+# Save new Username and Password (not done)
 
-
+#Menu Quit Function
 if Menu == quit:
     print("Stopping Program")
